f0c9d5d06fb0ca3441bd90151d78bae20c3d3351
sanvika125 <sakuntala.allu@gmail.com>
2024-12-28 17:51:13 +0530

    Added A B C Variables in ABC class from Dev1 Branch

--- Firstproject/src/icici/loans/carloans/ABC.java
+++ Firstproject/src/icici/loans/carloans/ABC.java
@@ -1,7 +1,7 @@
 package icici.loans.carloans;
 
 public class ABC {
-
+	int a, b, c ;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
